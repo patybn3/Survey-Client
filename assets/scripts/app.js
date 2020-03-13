bdccf5103383bfd8786e6c6a0c4e2c3fa48e5797
patybn3 <paty.nigro@gmail.com>
2020-03-12 21:51:53 -0400

    Added a button feature to survey's form that allow
    the user to add additional options as answers. Button is working and
    additional fields are working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,7 +7,9 @@ const authEvents = require('./auth/events')
 // run the app!
 $(() => {
 // hides and shows
+  $('#add-field').hide()
 
+  $('.button-add').on('click', surveyEvents.onAddField)
   surveyEvents.displayLoggedOutHome()
   surveyEvents.eventHandlers()
   authEvents.eventHandlers()
